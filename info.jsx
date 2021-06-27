e1553878ed38e531b256900baf706bdf3daf7a1a
Shruti2111 <86356528+Shruti2111@users.noreply.github.com>
2021-06-28 03:09:44 +0530

    Add files via upload

---
+++ info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>7 Days Bootcamp on JavaScript and React.js</h1>
+      <p>Thanks for participating!</p>
+    </div>
+  );
+}
+
+export default Info;
